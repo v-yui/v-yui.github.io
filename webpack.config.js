a4b10db13a4e76d8ab997f9d68d8379edfb31928
v-yui <1356674019@qq.com>
2021-02-18 23:08:25 +0800

    blog

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/main.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/main.html'),
+      filename: 'index.html',
+      showErrors: true,
+      inject: 'head',
+      chunks: ['main']
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      { test: /\.html$/i, loader: 'html-loader' },
+      { test: /\.md$/, use: ['html-loader', 'markdown-loader'] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+      { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+};
